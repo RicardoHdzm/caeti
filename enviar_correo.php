af6fe9b9e541e176b59da751cd39e1db90c86a4e
RicardoHdzm <jrhm95@gmail.com>
2025-05-05 13:11:03 -0700

    .

--- enviar_correo.php
+++ enviar_correo.php
@@ -1,26 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recogemos los datos del formulario
-    $name = htmlspecialchars($_POST['name']);
+    $nombre = htmlspecialchars($_POST['name']);
     $company = htmlspecialchars($_POST['company']);
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
     $mensaje = htmlspecialchars($_POST['mensaje']);
 
-    $para = "contacto@caeti.com.mx";  // 
+    $para = "contacto@caeti.com.mx";
+    $asunto = "Website: Nuevo Correo de: $company";
 
-    // Asunto del correo
-    $asunto = "Mensaje: $company";
-
-    // Cuerpo del correo
-    $cuerpo = "Nombre: $nombre\nCorreo electrónico: $email\nTeléfono: \n$phone\nMensaje: \n$mensaje";
-
-    // Cabecera del correo
-    $headers = "From: $email";  // Quién envía el correo
+    $cuerpo = "Nombre: $nombre\nCorreo electrónico: $email\nTeléfono: $phone\nMensaje: \n$mensaje";
+    $headers = "From: $email";
 
     // Enviamos el correo
     if (mail($para, $asunto, $cuerpo, $headers)) {
-        echo "¡Correo enviado con éxito!";
+        // Redireccionar si el correo fue enviado exitosamente
+        header("Location: https://caeti.com.mx/");
+        exit;
     } else {
         echo "Hubo un error al enviar el correo.";
     }
